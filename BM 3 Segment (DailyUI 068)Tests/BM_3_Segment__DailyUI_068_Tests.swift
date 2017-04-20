004ce7da589b7c9007f3f7c784c12f54f625cfa0
Mahmoud333 <Mahmoud@mahmouds-mini.zte.com.cn>
2017-04-20 13:59:14 +0200

    Initial Commit

---
+++ BM 3 Segment (DailyUI 068)Tests/BM_3_Segment__DailyUI_068_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  BM_3_Segment__DailyUI_068_Tests.swift
+//  BM 3 Segment (DailyUI 068)Tests
+//
+//  Created by Mahmoud Hamad on 4/20/17.
+//  Copyright © 2017 Mahmoud SMGL. All rights reserved.
+//
+
+import XCTest
+@testable import BM_3_Segment__DailyUI_068_
+
+class BM_3_Segment__DailyUI_068_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
